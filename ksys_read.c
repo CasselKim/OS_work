11fe417d44ff2cb1aa913a8fa103f499b90b88a5
CasselKim <seongr39@gmail.com>
2022-05-23 19:02:52 +0900

    Upload ksys_read, ksys_write

---
+++ ksys_read.c
@@ -0,0 +1,18 @@
+ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count)
+{
+	struct fd f = fdget_pos(fd);
+	ssize_t ret = -EBADF;
+
+	if (f.file) {
+		loff_t pos, *ppos = file_ppos(f.file);
+		if (ppos) {
+			pos = *ppos;
+			ppos = &pos;
+		}
+		ret = vfs_read(f.file, buf, count, ppos);
+		if (ret >= 0 && ppos)
+			f.file->f_pos = pos;
+		fdput_pos(f);
+	}
+	return ret;
+}
